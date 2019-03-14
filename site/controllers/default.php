83434a4699798144ce20f46f102bbcda14bab2ce
M. Folkers <hello@twobrain.io>
2019-03-14 15:59:10 +0100

    Adding boilerplates controller & model

---
+++ site/controllers/default.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+---------------------------------------
+Controllers
+---------------------------------------
+Templates should contain as little code as possible to separate
+logic and form. With template controllers you can add more logic
+to your templates without messing up your markup.
+
+See https://getkirby.com/docs/guide/templates/controllers
+*/
+
+return function($kirby, $site, $pages, $page) {
+
+  return [];
+};
---
+++ site/models/default.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+---------------------------------------
+Page models
+---------------------------------------
+Page models extend Kirby's default page object. Methods that
+you define in a page model are available everywhere in Kirby
+where you call a page of the extended type.
+
+See https://getkirby.com/docs/guide/templates/page-models
+*/
+
+class DefaultPage extends Page {}
